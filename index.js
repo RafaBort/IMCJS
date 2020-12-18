a1b5517f3c0861054a5585dff24381c91a5a6e89
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-12-18 02:45:11 -0300

    Exercício resolvido

---
+++ index.js
@@ -0,0 +1,11 @@
+Resultado();
+div.insertAdijacentHTML("beforeend", "<p>Diagnóstico: obesidade </p>");
+            }  
+        } else {
+             alert("Valor inválido!");
+        } 
+    } else {
+    alert("Valor inválido!");
+    }
+}
+Button.addEventListener("Click", buttonclick);
